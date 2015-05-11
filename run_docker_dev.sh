abdf087bac8ad236d26eb0a92d9f1a3234dbdf99
wti <wti@g.xsidekick.com>
2015-05-11 18:09:07 +0700

    add database.yml

--- run_docker_dev.sh
+++ run_docker_dev.sh
@@ -1,9 +1,11 @@
 PROJ=$(pwd)
+REDMINE=/opt/redmine
 
 docker run --rm -it \
 -h "xsk_redmine_dev" \
 --volumes-from redmine_data \
 -v $PROJ/redmine-3.0.3:/opt/redmine \
+-v $PROJ/config/database.yml:$REDMINE/config/database.yml \
 -v $PROJ/config/postgresql/postgresql-9.4:/etc/rc.d/init.d/postgresql-9.4 \
 -v $PROJ/config/nginx/nginx.init.d:/etc/rc.d/init.d/nginx \
 -v $PROJ/config/nginx/nginx.conf:/opt/nginx/conf/nginx.conf \
