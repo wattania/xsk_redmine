3edbf15224518a8a4eb69349b2f14ea9b7eadc02
Wattana <wattaint@gmail.com>
2015-05-11 07:09:43 -0400

    add port

--- run_docker_dev.sh
+++ run_docker_dev.sh
@@ -2,7 +2,9 @@ PROJ=$(pwd)
 
 docker run --rm -it \
 -h "xsk_redmine_dev" \
+-p 5432:5432 \
 --volumes-from redmine_data \
+-v $PROJ:/backup \
 -v $PROJ/redmine-3.0.3:/opt/redmine \
 -v $PROJ/config/postgresql/postgresql-9.4:/etc/rc.d/init.d/postgresql-9.4 \
 -v $PROJ/config/nginx/nginx.init.d:/etc/rc.d/init.d/nginx \
